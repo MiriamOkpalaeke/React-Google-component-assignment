1c3f3150d4169a733ea187d27234c5339e8c11de
Miriam C. Okpalaeke <Miriam.okpalaeke@gmail.com>
2023-04-12 13:02:54 +0100

    working on state

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -3,23 +3,39 @@ import SearchIcon from '../Images/download-removebg-preview (2).png';
 import SearchVoice from '../Images/pngwing.com.png'
 import SearchLens from '../Images/lens.PNG';
 import { useState } from 'react';
-import { getValue } from '@testing-library/user-event/dist/utils';
+
+let search = [];
+
  
 const SearchBar = () => {
-  const [enter, setEnter] = useState("0");
-  
-  const handleEnter = () => {
-     setEnter( "")
 
-  }
+     const [search, setSearch] = useState("");
+ 
+
+     const handleSearch = (event) => {
+        setSearch(event.target.value);
+     }
+
+
+
+   
+    
+  
+
+  
   return (
     <div className='SearchB'>
      <img className='sIcon' src={SearchIcon} alt="search"  width="15px" height="15px"/>
-      <input type="text" onChange={handleEnter}/>
-      <img  className='SVoice' src={SearchVoice} alt="voice" width="24px" />
+     <form > 
+      <input type="text" value={search} onChange={handleSearch}  />
+      </form>
+     
+      <img  className='SVoice' src={SearchVoice}  alt="voice" width="24px" />
       <img className='SLens' src={SearchLens} alt="lens" width="24px"/>
+
+     
     </div>
   )
 }
 
-export default SearchBar
+export default SearchBar;
