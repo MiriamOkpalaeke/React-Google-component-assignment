bc335eb7d0f0f90db270f05b757476dd9697c04a
Miriam C. Okpalaeke <Miriam.okpalaeke@gmail.com>
2023-04-25 15:26:09 +0100

    correction

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -4,21 +4,28 @@ import SearchVoice from '../Images/pngwing.com.png'
 import SearchLens from '../Images/lens.PNG';
 import { useState } from 'react';
 
-let search = [];
+
 
  
 const SearchBar = () => {
 
-     const [search, setSearch] = useState("");
+     const [search, setSearch] = useState(""); // to
+     const [updated, setUpdated] = useState("")
  
 
-     const handleSearch = (event) => {
+     const handleSearch = (event) => 
+     {   
         setSearch(event.target.value);
+       
+       ;
      }
+     // to target when the enter key is pressed
+   const handleKeyDown = (event) => {
+    if(event.key === "Enter") {
+    setUpdated(search);
+    }
+  }
 
-
-
-   
     
   
 
@@ -27,13 +34,13 @@ const SearchBar = () => {
     <div className='SearchB'>
      <img className='sIcon' src={SearchIcon} alt="search"  width="15px" height="15px"/>
      <form > 
-      <input type="text" value={search} onChange={handleSearch}  />
+      <input type="text" value={search} onChange={handleSearch} onKeyDown={handleKeyDown} />
       </form>
      
       <img  className='SVoice' src={SearchVoice}  alt="voice" width="24px" />
       <img className='SLens' src={SearchLens} alt="lens" width="24px"/>
 
-     
+     {updated}
     </div>
   )
 }
